80e0826e3ccb45051b5ebdc6535706d2d2399338
ascotai <206312751+ascotai@users.noreply.github.com>
2025-05-06 11:58:48 -0600

    add comment about lower num_ctx

--- config.py
+++ config.py
@@ -5,5 +5,6 @@ ANONYMIZED_TELEMETRY = "false"
 # LLM Configuration
 OLLAMA_MODEL = "msm" 
 OLLAMA_BASE_URL = "http://127.0.0.1:11434"
+#reduce to 16384 or 8192 if needed if low VRAM/RAM
 OLLAMA_NUM_CTX = 32768
 USE_VISION = False
