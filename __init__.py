a9c9b8ab7ba6c2ea53367f212071af8f8bb4294c
ascotai <206312751+ascotai@users.noreply.github.com>
2025-06-09 17:50:25 -0600

    set * imports

--- __init__.py
+++ __init__.py
@@ -2,3 +2,9 @@
 
 from .__main__ import generate_history_list
 from .code_generation import generate_playwright_code, generate_pytest_playwright_code
+
+__all__ = [
+    'generate_history_list',
+    'generate_playwright_code',
+    'generate_pytest_playwright_code'
+]
