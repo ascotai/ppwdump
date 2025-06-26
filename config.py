d911f607aa77f56427ebf218d5012cc967619135
ascotai <206312751+ascotai@users.noreply.github.com>
2025-06-26 12:51:50 -0600

    Fix default

--- config.py
+++ config.py
@@ -3,7 +3,7 @@
 BROWSER_MODEL = "msm32"
 CODE_MODEL = "msm32"
 BASE_URL = "http://127.0.0.1:11434/v1"
-USE_VISION = True
+USE_VISION = False
 ANONYMIZED_TELEMETRY = False
 ENABLE_MEMORY = False #not implemented leave False
 HEADLESS = False
