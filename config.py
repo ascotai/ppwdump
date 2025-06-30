021c9e656dc8e2b1180131c0f70d5ac8c014df8f
ascotai <206312751+ascotai@users.noreply.github.com>
2025-06-30 09:32:59 -0600

    support browser-use 0.4.2

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 # ppwdump/config.py
 
-BROWSER_MODEL = "msm32"
-CODE_MODEL = "msm32"
+BROWSER_MODEL = "msm"
+CODE_MODEL = "msm"
 BASE_URL = "http://127.0.0.1:11434/v1"
-USE_VISION = False #may be broken leave False for now
+USE_VISION = False
 ANONYMIZED_TELEMETRY = False
 ENABLE_MEMORY = False #not implemented leave False
 HEADLESS = False
