2069a9f1b75698c0f06ddba4e041a9ee3ae3d925
ascotai <206312751+ascotai@users.noreply.github.com>
2025-07-03 15:45:02 -0600

    reset defaults

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 # ppwdump/config.py
 #Ollama settings
 USE_CHAT_OLLAMA = True # True for better Ollama performance, False to use an OpenAI compatible provider
-OLLAMA_HOST = "192.168.105.3:11434"
+OLLAMA_HOST = "127.0.0.1:11434"
 #OpenAI settings (set USE_CHAT_OLLAMA = False)
 API_KEY = "none"
-OPENAI_BASE_URL = "http://192.168.105.3:11434/v1"
+OPENAI_BASE_URL = "http://127.0.0.1:11434/v1"
 #Both
 BROWSER_MODEL = "msm32"
 CODE_MODEL = "msm32"
