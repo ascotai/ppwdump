679e812a6aa2386c62d797629183dcc02e62365f
ascotai <206312751+ascotai@users.noreply.github.com>
2025-08-04 11:51:12 -0600

    update library example for vision

--- Examples/library/example_use.py
+++ Examples/library/example_use.py
@@ -5,13 +5,14 @@ from ppwdump import generate_history_list, generate_playwright_code, generate_py
 
 async def main():
     model_provider = "ollama"
-    my_browser_model = "msm32"
-    my_code_model = "msm32"
+    my_browser_model = "msmnew"
+    my_code_model = "msmnew"
     ollama_host  = "127.0.0.1:11434"
     headless = False
+    use_vision = True
     task = "Goto https://www.ecosia.org. Put Giant Panda in the search box. Click on the search button. Click on the link for the Giant Panda Wikipedia page. Finish."
     
-    history_list = await generate_history_list(task, model_provider=model_provider, model=my_browser_model, ollama_host=ollama_host, headless=headless)
+    history_list = await generate_history_list(task, model_provider=model_provider, model=my_browser_model, ollama_host=ollama_host, headless=headless,use_vision=use_vision)
     
     playwright_code_content = await generate_playwright_code(history_list, model_provider=model_provider, model=my_code_model, ollama_host=ollama_host)
     print("Playwright Code:\n")
